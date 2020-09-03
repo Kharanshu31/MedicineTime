dd7c867957cca3f1376d650973db2b1cb953ca03
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-09-04 01:44:05 +0530

    Adding logout/login

--- containers/Medicine/medicine.js
+++ containers/Medicine/medicine.js
@@ -1,11 +1,8 @@
-import React,{Component} from "react";
+import React,{Component,Fragment} from "react";
 import axios from "axios";
-import qs from "qs";
 import "./medicine.css";
-import { Route, Switch,NavLink,withRouter} from 'react-router-dom';
-import Course from "../../components/course/course";
-// import Button from "../../components/UI/Button/Button";
-// import Input from "../../components/UI/Input/Input";
+import {NavLink,Redirect,withRouter} from 'react-router-dom';
+import Swal from "sweetalert2";
 
 class Medicine extends Component {
 
@@ -13,21 +10,47 @@ class Medicine extends Component {
       names:[],
       times:[],
       time:"",
-      name:""
+      name:"",
+      username:"",
+      isLoggedIn:false
   }
 
   componentDidMount(){
 
-    axios.get("http://localhost:5000/user/")
-      .then(response=>{
-          if(response.data.length>0)
-          {
-              console.log(response.data);
-              this.setState({
-                names:response.data.map(key=>key.name),
-              })
-          }
+    const id=window.localStorage.getItem("_id")
+    console.log(id);
+    if(id === null || id.length === 0)
+    {
+      console.log("false");
+      this.setState({
+        isLoggedIn:false
       })
+    }
+    else
+    {
+      console.log("true");
+      axios.get(`http://localhost:5000/login/getUser?id=${window.localStorage.getItem("_id")}`)
+        .then(response=>{
+            if(response.data)
+            {
+                console.log(response.data);
+                this.setState({
+                  isLoggedIn:true
+                })
+            }
+        })
+    }
+
+  }
+
+  submitname=()=>{
+    // console.log(this.props);
+    this.props.history.push("/login");
+  }
+
+  handleChangeUsername=(event)=>{
+    let username=event.target.value;
+    this.setState({username:username});
   }
 
   handleChangeName=(event)=>{
@@ -44,100 +67,105 @@ class Medicine extends Component {
 
    event.preventDefault();
 
-  const newArray=[...this.state.names];
-
-  newArray.push(this.state.name+" ");
+   if(this.state.name ==="" || this.state.time==="")
+   {
+     //alert("Please fill all the fields")
+     Swal.fire("Error","Please fill all the fields")
+     return;
+   }
 
   const userItems={
-    name:this.state.name
+    name:this.state.name,
+    time:this.state.time,
+    username:this.state.username
   }
 
-  let axiosConfig = {
-  headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      "Access-Control-Allow-Origin": "*",
-  }
-};
-
-
-
-// var params = new URLSearchParams();
-// params.append("name",this.state.name);
-// console.log(params);
 
   axios.post("http://localhost:5000/user/add",userItems)
-    .then(res=>console.log(res))
+    .then(res=>console.log(res.data))
       .catch(error=>console.log(error.response));
 
+    this.props.history.push("/course");
 
-//   axios({
-//     method: 'post',
-//     url: '/user/add',
-//     data:{
-//     name:this.state.name
-//   }
-// }).then(response=>console.log(response.data))
-//   .catch(err=>console.log(err));
-
-  this.setState({names:newArray},()=>{
-    const queryparam=[];
-    for(let i in this.state.names){
-      queryparam.push("name="+encodeURIComponent(this.state.names[i]))
-    }
-    const queryString=queryparam.join("&");
-    this.props.history.push({
-      pathname:"/course",
-      search:"?"+queryString
-    })
-  })
-
-
-
+  // this.setState({names:newArray},()=>{
+  //   const queryparam=[];
+  //   for(let i in this.state.names){
+  //     queryparam.push("name="+encodeURIComponent(this.state.names[i]))
+  //   }
+  //   const queryString=queryparam.join("&");
+  //   this.props.history.push({
+  //     pathname:"/course",
+  //     search:"?"+queryString
+  //   })
+  // })
 
   }
 
   render(){
+        //   <div className="Navbar">
+        //   <ul className="NavigationItems">
+        //     <li className="NavigationItem">
+        //       <nav>
+        //         <NavLink to="/course" activeClassName="active">Course</NavLink>
+        //         <NavLink to="/login" activeClassName="active">Login</NavLink>
+        //       </nav>
+        //     </li>
+        //   </ul>
+        // </div>
+        //
+
+    const authLinks=(
+      <Fragment>
+        <a href="#" className="loginLink" onClick={(e)=>{
+            e.preventDefault();
+            window.localStorage.removeItem("_id");
+            window.location.href="/";
+        }}>Logout</a>
+      </Fragment>
+    )
+
+    const guestLinks=(
+      <Fragment>
+        <NavLink to="/login" activeClassName="active loginLink">Login</NavLink>
+        <NavLink to="/register" activeClassName="active loginLink">Register</NavLink>
+      </Fragment>
+    )
 
     return(
       <div className="whole">
 
-      <div className="Navbar">
-      <ul className="NavigationItems">
-        <li className="NavigationItem">
-          <nav>
-            <NavLink to="/course" activeClassName="active">Course</NavLink>
-          </nav>
-        </li>
-      </ul>
-    </div>
-
-
       <div className="container">
 
+      <li className="NavigationItem">
+        <nav>
+          <NavLink to="/course" activeClassName="active">Course</NavLink>
+          {this.state.isLoggedIn ? (authLinks) : (guestLinks) }
+        </nav>
+      </li>
 
 
           <div>
             <h1 className="heading">MEDICINE TIME</h1>
-            <input className="Username" placeholder="Name"/>
-            <button>Submit</button>
+            <input className="Username" placeholder="Name" onChange={this.handleChangeUsername}/>
+            <button className="Userbtn" onClick={this.submitname}>Submit</button>
           </div>
       </div>
 
-    <div className="InfoContainer">
+    <div className="InfoContainer DirectionContainer">
 
       <div className="InputContainer">
-      <h3 >Enter the Medicine Name</h3>
+      <h3>Enter the Medicine Name</h3>
       <input className="Info" onChange={this.handleChangeName}></input>
-      <button className="login" onClick={this.onsetMedicine}>Go</button>
       </div>
 
       <div className="InputContainer">
       <h3>Enter the Time</h3>
       <input className="Info" onChange={this.handleChangeTime}></input>
-      <button className="login">Go</button>
       </div>
 
     </div>
+
+      <button className="login" onClick={this.onsetMedicine}>Submit</button>
 
       </div>
     )
